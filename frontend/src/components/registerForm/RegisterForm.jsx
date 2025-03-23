dbfd581713da443edadd053d46a6ebd2fd4e66ab
Halyna13890 <galinastudio9@gmail.com>
2025-03-23 14:30:36 +0100

    added css register form

--- frontend/src/components/registerForm/RegisterForm.jsx
+++ frontend/src/components/registerForm/RegisterForm.jsx
@@ -5,6 +5,7 @@ import { registerUser } from "../../redux/slieces/registerSlice";
 import { useNavigate } from "react-router-dom";
 import ichgram from "../../accets/ICHGRA.jpg"
 import { Link } from "react-router-dom"
+import "./registerForm.css"
 
 const RegisterForm = () => {
     const dispatch = useDispatch();
@@ -31,10 +32,14 @@ const RegisterForm = () => {
     return (
         <div>
             <div>
-                <img src={ichgram} alt="Ichgram" />
-                <h5>Sign up to see photos and videos from your friends.</h5>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <input
+                
+                <form className="register-form" onSubmit={handleSubmit(onSubmit)}>
+                    <img src={ichgram} alt="Ichgram" />
+                    <h5 className="register-promo-text">Sign up to see photos and videos from your friends.</h5>
+                  
+                   <div className="register-input-area">
+                   <input
+                    className="register-input"
                         type="email"
                         {...register("email", {
                             required: "Email is required",
@@ -44,13 +49,18 @@ const RegisterForm = () => {
                     />
                     {errors.email && <p>{errors.email.message}</p>}
 
-                    <input {...register("fullName", { required: "Full name is required" })} placeholder="Full Name" />
+                    <input 
+                    className="register-input"
+                    {...register("fullName", { required: "Full name is required" })} placeholder="Full Name" />
                     {errors.fullname && <p>{errors.fullname.message}</p>}
 
-                    <input {...register("username", { required: "Username is required" })} placeholder="Username" />
+                    <input 
+                    className="register-input"
+                    {...register("username", { required: "Username is required" })} placeholder="Username" />
                     {errors.username && <p>{errors.username.message}</p>}
 
                     <input
+                    className="register-input"
                         type="password"
                         {...register("password", {
                             required: "Password is required",
@@ -60,20 +70,19 @@ const RegisterForm = () => {
                     />
                     {errors.password && <p>{errors.password.message}</p>}
 
-                    <div>
-                        <h5>People who use our service may have uploaded your contact information to Instagram. Learn More</h5>
-                    </div>
+                   </div>
+                    <div className="info-btn-area">
+                    <h5>People who use our service may have uploaded your contact information to Instagram. <span class="highlight">Learn More</span></h5>
+                    <h5>By signing up, you agree to our <span class="highlight">Terms</span>, <span class="highlight">Privacy Policy</span> and <span class="highlight">Cookies Policy</span></h5>
 
-                    <div>
-                        <h5>By signing up, you agree to our Terms, Privacy Policy and Cookies Policy</h5>
+                        <button className="register-btn"type="submit" disabled={loading}>{loading ? "Loading..." : "Register"}</button>
                     </div>
-
-                    <button type="submit" disabled={loading}>{loading ? "Loading..." : "Register"}</button>
+                    
                 </form>
             </div>
 
-            <div>
-                <h4>Have an account? <Link to="/login">Log in</Link></h4>
+            <div className="login-link-area">
+                <h4>Have an account? <Link  className="login-link"to="/login">Log in</Link></h4>
             </div>
         </div>
     );
