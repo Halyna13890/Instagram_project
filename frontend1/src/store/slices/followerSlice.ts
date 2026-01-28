496be9091beed2b0e1f53156ccee5899ec3d8038
Halyna13890 <galinastudio9@gmail.com>
2026-01-28 09:22:26 +0100

    add follower files

---
+++ frontend1/src/store/slices/followerSlice.ts
@@ -0,0 +1,6 @@
+import { createSlice,  createAsyncThunk, rejectWithValue } from "@reduxjs/toolkit";
+import type {PayloadAction} from "@reduxjs/toolkit"
+import axios from "axios";
+import api from "@/api/interceptor";
+import Cookies from "js-cookie";
+import type { ApiError, ApiMessageResponse } from "@/types/apiRequestsType";
